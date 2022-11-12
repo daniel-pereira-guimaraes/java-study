67bfec7157ab713ea67b2366f12cef67ed70e59d
Daniel Pereira Guimarães <danielpgbrasil@gmail.com>
2022-11-12 11:26:46 -0400

    Added printMethodName on MiscStudy class.

--- src/main/java/java_study/MiscStudy.java
+++ src/main/java/java_study/MiscStudy.java
@@ -4,14 +4,20 @@ final public class MiscStudy {
 	
 	public static void main(String[] args) {
 		testGetMethodName();
+		printMethodName();
 	}
 	
 	public static String getMethodName() {
 		return Thread.currentThread().getStackTrace()[2].getMethodName();
 	}
 	
+	public static void printMethodName() {
+		String methodName = Thread.currentThread().getStackTrace()[2].getMethodName();
+		System.out.println("--- " + methodName + " ---");
+	}
+	
 	private static void testGetMethodName() {
 		System.out.println("Method name: " + getMethodName());
 	}
-
+	
 }
