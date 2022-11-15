f1d404683100d35a6b443e084383488417a3a732
Daniel Pereira Guimarães <danielpgbrasil@gmail.com>
2022-11-15 19:37:58 -0400

    Added printNumberList method on GenericStudy1 class.

--- src/main/java/java_study/GenericStudy1.java
+++ src/main/java/java_study/GenericStudy1.java
@@ -8,20 +8,33 @@ public class GenericStudy1 {
 	public static void main(String[] args) {
 		
 		List<Integer> integerList = Arrays.asList(10, 20, 30, 40, 50);
+		List<Double> doubleList = Arrays.asList(2.0, 3.5, 5.2, 10.3);
 		List<String> stringList = Arrays.asList("Ana", "Jhon", "Joe", "Mary");
 		List<?> superList1 = integerList;
 		List<?> superList2 = stringList;
+		List<? extends Number> superList3 = doubleList;
 
 		printList(integerList);
 		printList(stringList);
 		printList(superList1);
 		printList(superList2);
+		
+		printNumberList(integerList);
+		printNumberList(doubleList);
+		printNumberList(superList3);
 	}
 	
 	private static void printList(List<?> list) {
 		MiscStudy.printMethodName();
 		for (Object item : list)
 			System.out.println(item);
+		System.out.println();
+	}
+	
+	private static void printNumberList(List<? extends Number> list) {
+		MiscStudy.printMethodName();
+		for (Number n : list)
+			System.out.println("2 x " + n + " = " + 2 * n.doubleValue());
 		System.out.println();
 	}
 	
