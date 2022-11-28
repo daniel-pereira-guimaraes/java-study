d0058e8440f71aa7e3ac8dcf95ab456fd029c08e
Daniel Pereira Guimarães <danielpgbrasil@gmail.com>
2022-11-27 20:50:55 -0400

    Added ProductDTO class.

---
+++ src/main/java/java_study/ProductDTO.java
@@ -0,0 +1,34 @@
+package java_study;
+
+public class ProductDTO {
+
+	private String name;
+	private Double price;
+	
+	public ProductDTO() {
+		
+	}
+
+	public ProductDTO(String name, Double price) {
+		super();
+		this.name = name;
+		this.price = price;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+	
+}
