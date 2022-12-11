5a27ca5a2a56236dbe0e4a44a53d667c6b3a8006
Daniel Pereira Guimarães <danielpgbrasil@gmail.com>
2022-12-10 20:16:47 -0400

    Added enumWithCustomValues method on EnumStudy class.

--- src/main/java/java_study/EnumStudy.java
+++ src/main/java/java_study/EnumStudy.java
@@ -5,6 +5,7 @@ public class EnumStudy {
 	public static void main(String[] args) {
 		
 		basicEnumOperations();
+		enumWithCustomValues();
 		
 		
 	}
@@ -44,5 +45,39 @@ public class EnumStudy {
 		
 		System.out.println();
 	}
+
 	
+	private static enum YearSeasons {
+		SPRING(1),
+		SUMMER(2),
+		AUTUMN(3),
+		WINTER(4);
+		
+		public int value;
+
+		YearSeasons(int value) {
+			this.value = value;
+		}
+	}
+	
+	private static void enumWithCustomValues() {
+		MiscStudy.printMethodName();
+		
+		YearSeasons seasons1 = YearSeasons.SPRING;
+		YearSeasons seasons2 = YearSeasons.WINTER;
+		
+		System.out.println("seasons1: " + seasons1);
+		System.out.println("seasons2: " + seasons2);
+
+		System.out.println("seasons1.name: " + seasons1.name());
+		System.out.println("seasons2.name: " + seasons2.name());
+		
+		System.out.println("seasons1.value: " + seasons1.value);
+		System.out.println("seasons2.value: " + seasons2.value);
+		
+		System.out.println("seasons1.ordinal(): " + seasons1.ordinal());
+		System.out.println("seasons2.ordinal(): " + seasons2.ordinal());
+	}
 }
+
+
