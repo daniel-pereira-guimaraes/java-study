08d0eaa4be8cd478854e8b84bf24c8d017422b00
Daniel Pereira Guimarães <danielpgbrasil@gmail.com>
2022-12-14 20:34:51 -0400

    Added cloneWithGson method on CloneStudy class.

--- src/main/java/java_study/CloneStudy.java
+++ src/main/java/java_study/CloneStudy.java
@@ -2,10 +2,13 @@ package java_study;
 
 import org.apache.commons.lang3.SerializationUtils;
 
+import com.google.gson.Gson;
+
 public class CloneStudy {
 	
 	public static void main(String[] args) {
 		cloneWidhSerialization();
+		cloneWidhGson();
 	}
 	
 	private static void cloneWidhSerialization() {
@@ -24,5 +27,24 @@ public class CloneStudy {
 		System.out.println(p3);
 		System.out.println();
 	}
-			
+
+	private static void cloneWidhGson() {
+		MiscStudy.printMethodName();
+		
+		Product p1 = new Product(100L, "Keyboard", 15.0, 10, new ProductGroup(1L, "Eletronics"));
+
+		Gson gson = new Gson();
+		Product p2 = gson.fromJson(gson.toJson(p1), Product.class);
+		Product p3 = gson.fromJson(gson.toJson(p1), Product.class);
+
+		p3.setId(200L);
+		p3.setName("Kite");
+		p3.getGroup().setName("Toys");
+		
+		System.out.println(p1);
+		System.out.println(p2);
+		System.out.println(p3);
+		System.out.println();
+	}
+	
 }
