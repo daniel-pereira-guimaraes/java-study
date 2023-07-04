772d8d4e16db514f14424848fba6cf5416e5d351
Daniel Pereira Guimarães <danielpgbrasil@gmail.com>
2023-07-04 07:06:36 -0400

    Added Base64Study class, with encodeDecodeBase64 method.

---
+++ src/main/java/java_study/Base64Study.java
@@ -0,0 +1,26 @@
+package java_study;
+
+import java.io.UnsupportedEncodingException;
+import java.util.Base64;
+
+public class Base64Study {
+
+	public static void main(String[] args) throws Throwable {
+
+		encodeDecodeBase64();
+		
+	}
+	
+	private static void encodeDecodeBase64() throws UnsupportedEncodingException {
+		MiscStudy.printMethodName();
+		
+		final String input = "I like Java";
+		final String encoded = Base64.getEncoder().encodeToString(input.getBytes("utf-8"));
+		final String decoded = new String(Base64.getDecoder().decode(encoded.getBytes()));
+		
+		System.out.println("\tinput: " + input);
+		System.out.println("\tencoded: " + encoded);
+		System.out.println("\tdecoded: " + decoded);
+		System.out.println();
+	}
+}
