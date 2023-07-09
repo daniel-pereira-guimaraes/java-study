fda5eb77299393f1a8b27af61724a65db557bd01
Daniel Pereira Guimarães <danielpgbrasil@gmail.com>
2023-07-09 10:21:27 -0400

    Added JavaScript running example.

---
+++ src/main/java/java_study/ScriptEngineStudy.java
@@ -0,0 +1,30 @@
+package java_study;
+
+import javax.script.Bindings;
+import javax.script.ScriptContext;
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+
+public class ScriptEngineStudy {
+	
+	public static void main(String[] args) throws ScriptException {
+		
+		final String engineName = "Nashorn";
+		final ScriptEngineManager engineManager = new ScriptEngineManager();
+		final ScriptEngine engine = engineManager.getEngineByName(engineName);
+		final Bindings bind = engine.getBindings(ScriptContext.ENGINE_SCOPE);
+		bind.put("msg", "Hello JavaScript!");
+		final String script = 
+				"print(msg); " +
+				"var a = 5; " +
+				"var b = 10; " +
+				"var c = a + b; " +
+				"print('a: ' + a); " +
+				"print('b: ' + b); " +
+				"print('c: ' + c); " +
+				"print('Goodbye!');";
+		engine.eval(script);
+	}
+
+}
