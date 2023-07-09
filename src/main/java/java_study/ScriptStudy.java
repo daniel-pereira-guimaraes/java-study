282b65b0cde9077f254eea186ad050c45f1e1263
Daniel Pereira Guimarães <danielpgbrasil@gmail.com>
2023-07-09 11:36:41 -0400

    Added rhinoCalc + rhinoCalcExample methods to ScriptStudy class.

--- src/main/java/java_study/ScriptStudy.java
+++ src/main/java/java_study/ScriptStudy.java
@@ -6,29 +6,56 @@ import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
 import javax.script.ScriptException;
 
+import org.mozilla.javascript.Context;
+import org.mozilla.javascript.ContextFactory;
+import org.mozilla.javascript.Scriptable;
+
 public class ScriptStudy {
-	
+
 	public static void main(String[] args) throws ScriptException {
 		nashornExample();
+		rhinoCalcExample();
 	}
-	
+
 	private static void nashornExample() throws ScriptException {
 		MiscStudy.printMethodName();
+		
 		final String engineName = "Nashorn";
 		final ScriptEngineManager engineManager = new ScriptEngineManager();
 		final ScriptEngine engine = engineManager.getEngineByName(engineName);
 		final Bindings bind = engine.getBindings(ScriptContext.ENGINE_SCOPE);
-		bind.put("msg", "Hello JavaScript!");
-		final String script = 
-				"print(msg); " +
-				"var a = 5; " +
-				"var b = 10; " +
-				"var c = a + b; " +
-				"print('a: ' + a); " +
-				"print('b: ' + b); " +
-				"print('c: ' + c); " +
+		final String script =
+				"print(msg);" +
+				"var a = 5;" +
+				"var b = 10;" +
+				"var c = a + b;" +
+				"print('a: ' + a);" +
+				"print('b: ' + b);" +
+				"print('c: ' + c);" +
 				"print('Goodbye!');";
+		
+		bind.put("msg", "Hello JavaScript!");
 		engine.eval(script);
+		System.out.println();
+	}
+
+	@SuppressWarnings("unchecked")
+	private static <T> T rhinoCalc(String script) {
+		Context context = ContextFactory.getGlobal().enterContext();
+		try {
+			final Scriptable scope = context.initStandardObjects();
+			return (T) context.evaluateString(scope, script, null, 0, null);
+		} finally {
+			Context.exit();
+		}
+	}
+	
+	private static void rhinoCalcExample() {
+		MiscStudy.printMethodName();
+		final String script = "20 + Math.log(5)";
+		final Double result = rhinoCalc(script);
+		System.out.println("Script: " + script);
+		System.out.println("Result: " + result);
 		System.out.println();
 	}
 
